acabe1927cf52fd7b04a38792672bed6d1110b69
Emizzari <emilie@tonnessen.com>
2020-10-07 13:43:29 +0200

    Adding a "clear all" btn

---
+++ js/favourites.js
@@ -0,0 +1,37 @@
+import { getExistingFavs } from "./components/storage/existingFavs.js";
+
+function createFavList (){
+    const favourites = getExistingFavs();
+
+    const productContainer = document.querySelector(".product-container");
+
+    if (favourites.length === 0) {
+      productContainer.innerHTML = "No favourites yet";
+    }
+
+    favourites.forEach((favourite) => {
+      productContainer.innerHTML += `
+        <div class="product col-sm-12 col-md-6 col-lg-3">
+                <img src="${favourite.image}" alt="${favourite.name}" class="product__img">
+                <h4>${favourite.name}</h4>
+                <p>${favourite.price} €</p>
+                <p>${favourite.description}</p>
+                <i class="fa fa-heart" data-id="${favourite.id}" data-name="${favourite.name}"></i>
+            </div>
+    `;
+    });
+}
+
+createFavList();
+
+// Clear localStorage
+document
+  .querySelector(".clear-storage button")
+  .addEventListener("click", handleClearBtnClick);
+
+
+function handleClearBtnClick() {
+    localStorage.clear();
+
+    createFavList();
+}

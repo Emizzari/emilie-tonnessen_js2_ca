bc73bdf17fcc8a955e131da98fc021569e21b280
Emizzari <emilie@tonnessen.com>
2020-10-07 13:43:14 +0200

    Finishing up localStorage

--- js/components/html/createHTML.js
+++ js/components/html/createHTML.js
@@ -18,8 +18,6 @@ export function createHTML(products) {
         let cssClass = "far";
 
         const doesObjectExist = favourites.find(function (fav) {
-            console.log(fav);
-
             return parseInt(fav.id) === product.id;
         });
 
@@ -31,12 +29,55 @@ export function createHTML(products) {
             <div class="product col-sm-12 col-md-6 col-lg-3">
                 <img src="http://localhost:1337${product.image.formats.medium.url}" alt="${product.name}" class="product__img">
                 <h4>${product.name}</h4>
-                <p>${product.price} €</p>
+                <p>${product.price}</p>
                 <p>${product.description} €</p>
-                <div class="heart-icon">
-                    <i class="${cssClass} fa-heart" data-id="${product.id}" data-name="${product.name}"></i>
-                </div>
+                <i class="${cssClass} fa-heart" data-id="${product.id}" data-name="${product.name}" data-description="${product.description}" data-image="http://localhost:1337${product.image.formats.medium.url}" data-price="${product.price}"></i>
             </div>
         `;
     });
+
+    // Favourites:
+    const favButtons = document.querySelectorAll(".product i");
+    // console.log(favButtons);
+
+    favButtons.forEach((button) => {
+      button.addEventListener("click", handleFavClick);
+    });
+
+    function handleFavClick() {
+      this.classList.toggle("fa");
+      this.classList.toggle("far");
+
+      const id = this.dataset.id;
+      const name = this.dataset.name;
+      const price = this.dataset.price;
+      const image = this.dataset.image;
+      const description = this.dataset.description;
+
+      const currentFavs = getExistingFavs();
+
+      const productExists = currentFavs.find(function (fav) {
+        return fav.id === id;
+      });
+
+      if (productExists === undefined) {
+        const product = {
+            id: id,
+            name: name,
+            price: price,
+            image: image,
+            description: description
+        };
+
+        currentFavs.push(product);
+        saveFavs(currentFavs);
+      } else {
+        const newFavs = currentFavs.filter((fav) => fav.id !== id);
+        saveFavs(newFavs);
+      }
+    }
+
+    function saveFavs(favs) {
+      localStorage.setItem("favourites", JSON.stringify(favs));
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-// import { getExistingFavs } from "./components/storage/existingFavs.js";
 import { productsURL } from "./settings/api.js";
-// import { createHTML } from "./components/html/createHTML.js";
+import { createHTML } from "./components/html/createHTML.js";
 import displayMessage from "./components/messages/displayMessage.js";
 import { searchProducts } from "./components/search/searchProducts.js";
 
@@ -12,7 +11,6 @@ async function fetchAPI() {
         const json = await response.json();
         const product = json;
 
-        console.log(product);
         createHTML(product);
         searchProducts(product);
 
@@ -23,81 +21,3 @@ async function fetchAPI() {
 }
 
 fetchAPI();
-
-// Temporarly solution:
-function createHTML(products) {
-  const productContainer = document.querySelector(".product-container");
-
-  productContainer.innerHTML = "";
-
-  if (products.length === 0) {
-    displayMessage("", emptyFilter, ".product-container");
-  }
-
-  // const favourites = getExistingFavs();
-
-  products.forEach(function (product) {
-    /* let cssClass = "far";
-
-    const doesObjectExist = favourites.find(function (fav) {
-      console.log(fav);
-
-      return parseInt(fav.id) === product.id;
-    });
-
-    if (doesObjectExist) {
-      cssClass = "fa";
-    } */
-
-    productContainer.innerHTML += `
-            <div class="product col-sm-12 col-md-6 col-lg-3">
-                <img src="http://localhost:1337${product.image.formats.medium.url}" alt="${product.name}" class="product__img">
-                <h4>${product.name}</h4>
-                <p>${product.price} €</p>
-                <p>${product.description} €</p>
-                
-                <i class="far fa-heart" data-id="${product.id}" data-name="${product.name}"></i>
-                
-            </div>
-        `;
-  });
-}
-
-
-// Favourites:
-const favButtons = document.querySelectorAll(".product i");
-
-console.log(favButtons);
-
-/*favButtons.forEach((button) => {
-    button.addEventListener("click", handleFavClick);
-});
-
- function handleFavClick() {
-    console.log(event);
-
-    this.classList.toggle("fa");
-    this.classList.toggle("far");
-
-    const id = this.dataset.id;
-    const name = this.dataset.name;
-
-    const currentFavs = getExistingFavs();
-
-    const productExists = currentFavs.find(function (fav) {
-        return fav.id === id;
-    });
-
-    if (productExists === undefined) {
-        const product = { id: id, name: name };
-        currentFavs.push(product);
-        saveFavs(currentFavs);
-    } else {
-        const newFavs = currentFavs.filter((fav) => fav.id !== id);
-        saveFavs(newFavs);
-    }
-}
-
-function saveFavs(favs) {
-    localStorage.setItem("favourites", JSON.stringify(favs));
-} */
